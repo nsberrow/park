901db060be682d6f43aa85051b0e6ea616a02231
nsberrow <nsberrow@gmail.com>
2015-07-03 21:12:22 +0200

    Columns

--- templates/page-sidebar-left.php
+++ templates/page-sidebar-left.php
@@ -4,7 +4,7 @@ Template Name: Left Sidebar
 */
 get_header(); ?>
 <div class="row">
-    <div class="small-12 large-8 large-push-4 columns" role="main">
+    <div class="small-12 large-9 large-push-4 columns" role="main">
 
         <?php do_action( 'foundationpress_before_content' ); ?>
 
