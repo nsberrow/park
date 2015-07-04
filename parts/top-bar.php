8fef77c07d5b94a3ea3d9d8a506e9ed5c5ed083e
nsberrow <nsberrow@gmail.com>
2015-07-04 09:17:54 +0200

    Responsive

--- parts/top-bar.php
+++ parts/top-bar.php
@@ -9,7 +9,7 @@
 
 ?>
 <div class="row">
-  <div class="top-bar-container contain-to-grid show-for-medium-up">
+  <div class="top-bar-container contain-to-grid show-for-large-up">
       <nav class="top-bar" data-topbar role="navigation">
           <ul class="title-area">
               <li class="name">
