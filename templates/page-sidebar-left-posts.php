344d72563919b02e51bfb911d0fe8e5d5c783537
nsberrow <nsberrow@gmail.com>
2015-07-16 14:12:03 +0200

    News section

--- templates/page-sidebar-left-posts.php
+++ templates/page-sidebar-left-posts.php
@@ -11,19 +11,18 @@ get_header(); ?>
       <?php $the_query = new WP_Query( 'posts_per_page=15' ); ?>
 
       <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-
         <h1 class="entry-title">
-            <a href="<?php the_permalink(); ?>">
-                <?php the_title(); ?>
-            </a>
+          <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
         </h1>
-        <article id="post-<?php the_ID(); ?>" <?php post_class( 'category-listing' ); ?>>
-                <?php if ( has_post_thumbnail() ) { ?>
-                    <a href="<?php the_permalink(); ?>">
-                        <?php the_post_thumbnail( 'thumbnail' ); ?>
-                    </a>
-                    <?php the_excerpt(__('(more…)')); ?>
-                <?php } ?>
+        <?php if ( has_post_thumbnail() ) { ?>
+            <a href="<?php the_permalink(); ?>">
+                <?php the_post_thumbnail( 'thumbnail' ); ?>
+            </a>
+        <?php } ?>
+        <p><?php the_excerpt(__('(more…)')); ?></p>
+
+
+
       <?php
         endwhile;
       wp_reset_postdata();
