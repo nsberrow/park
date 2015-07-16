1e4328bfdf11b140d3fbcc882e5dc480177023c4
nsberrow <nsberrow@gmail.com>
2015-07-16 14:16:18 +0200

    News section

--- templates/page-sidebar-left-posts.php
+++ templates/page-sidebar-left-posts.php
@@ -5,21 +5,23 @@ Template Name: Left Sidebar Posts
 get_header(); ?>
 <div class="row">
   <div class="small-12 medium-12 large-9 large-push-3 columns" role="main">
-
+    <h1 class="entry-title">
+      Parklands Headlines
+    </h1>
     <ul>
-
+      <li>
       <?php $the_query = new WP_Query( 'posts_per_page=15' ); ?>
 
       <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-        <h1 class="entry-title">
+        <h2 class="entry-title">
           <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-        </h1>
+        </h2>
         <?php if ( has_post_thumbnail() ) { ?>
             <a href="<?php the_permalink(); ?>">
                 <?php the_post_thumbnail( 'thumbnail' ); ?>
             </a>
         <?php } ?>
-        <p><?php the_excerpt(__('(more…)')); ?></p>
+      <a class="button" href="<?php the_permalink() ?>">Read more</a>
 
 
 
@@ -27,6 +29,7 @@ get_header(); ?>
         endwhile;
       wp_reset_postdata();
       ?>
+    </li>
     </ul>
 
   </div>
