21a46a7b42bd1c64b8f4a068f15fe4c10a0e5a95
nsberrow <nsberrow@gmail.com>
2015-07-16 14:02:26 +0200

    News section

--- templates/page-sidebar-left-posts.php
+++ templates/page-sidebar-left-posts.php
@@ -4,34 +4,29 @@ Template Name: Left Sidebar Posts
 */
 get_header(); ?>
 <div class="row">
-    <div class="small-12 medium-12 large-9 large-push-3 columns" role="main">
+  <div class="small-12 medium-12 large-9 large-push-3 columns" role="main">
 
-        <?php do_action( 'foundationpress_before_content' ); ?>
+    <ul>
+      // Define our WP Query Parameters 
+      <?php $the_query = new WP_Query( 'posts_per_page=5' ); ?>
 
+      // Start our WP Query
+      <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+
+        // Display the Post Title with Hyperlink
+        <li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
+
+        // Display the Post Excerpt
+        <li><?php the_excerpt(__('(more…)')); ?></li>
+
+        // Repeat the process and reset once it hits the limit
         <?php
-        $query = new WP_Query( 'cat=-15' );
-        while ( have_posts() ) : the_post(); ?>
-            <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-                <header>
-                    <h1 class="entry-title"><?php the_title(); ?></h1>
-                </header>
-                <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-                <div class="entry-content">
-                    <?php the_content(); ?>
-                </div>
-                <footer>
-                    <?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-                    <p><?php the_tags(); ?></p>
-                </footer>
-                <?php do_action( 'foundationpress_page_before_comments' ); ?>
-                <?php comments_template(); ?>
-                <?php do_action( 'foundationpress_page_after_comments' ); ?>
-            </article>
-        <?php endwhile;?>
+      endwhile;
+      wp_reset_postdata();
+      ?>
+    </ul>
 
-        <?php do_action( 'foundationpress_after_content' ); ?>
-
-    </div>
-    <?php get_sidebar( 'left' ); ?>
+  </div>
+  <?php get_sidebar( 'left' ); ?>
 </div>
 <?php get_footer(); ?>
